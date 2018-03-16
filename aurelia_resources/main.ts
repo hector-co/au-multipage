3f81e15a2b9c02adf66d523522f5f9a652c24d68
Hector Carballo <hectorcarballo.hco@gmail.com>
2018-03-16 11:32:39 -0400

    adjusts for enhance only existent elements

--- aurelia_resources/main.ts
+++ aurelia_resources/main.ts
@@ -1,6 +1,10 @@
 import { Aurelia, PLATFORM, FrameworkConfiguration } from 'aurelia-framework'
 import environment from './environment';
 
+let elements = [
+  { name: 'test-element', selector: 'test-element' }
+];
+
 export async function configure(aurelia: Aurelia) {
   aurelia.use
     .basicConfiguration();
@@ -15,10 +19,17 @@ export async function configure(aurelia: Aurelia) {
     aurelia.use.plugin('aurelia-testing');
   }
 
-  await aurelia.start();
-  aurelia.enhance();
+  await aurelia.use.apply();
+
+  elements.forEach(e => {
+    var domElements = document.querySelectorAll(e.selector);
+    for (var i = 0; i < domElements.length; i++)
+      aurelia.enhance(domElements[i]);
+  });
 }
 
 export function configureResources(config: FrameworkConfiguration) {
-  config.globalResources([PLATFORM.moduleName('elements/test-element')]);
+  config.globalResources(
+    elements.map(e => PLATFORM.moduleName(`elements/${e.name}`))
+  );
 }
